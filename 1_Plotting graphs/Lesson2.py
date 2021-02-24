9f1b5447cfa92ecffb59c55f8de5d70c1491c7a5
BalsevichDmintriy <Baltsevich@yandex.ru>
2021-02-24 10:21:57 +0700

    Lesson2 finished

--- 1_Plotting graphs/Lesson2.py
+++ 1_Plotting graphs/Lesson2.py
@@ -2,6 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-x = np.arange(-10, 10.01, 0.01)
-plt.plot(x, x**2)
-plt.show()
+def graph1():
+    """ This function creates the graph y(x)=x*x-x-6 and finds out root y(x)=0"""
+    x = np.arange(0, 6, 1)
+    plt.xlabel('x')
+    plt.ylabel('y(x)=x*x - x - 6')
+    plt.title('График y(x)=x*x-x-6', fontsize=16)
+    plt.grid(color='gray')
+    plt.text(1.9, 0.85, 'y(x)=0',
+             fontsize=18, bbox={'facecolor': 'yellow'})
+    plt.plot(x * x - x - 6, 'r-o')
+    plt.plot(3, 0, 'bs')
+    plt.show()
+
+
+graph1()
