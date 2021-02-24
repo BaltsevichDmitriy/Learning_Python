f9319f43456d858bb280e3d3130372e392d01064
BaltsevichDmitriy <baltsevich@yandex.ru>
2021-02-24 22:04:12 +0700

    Add module "class2" with part filling.

--- 1_Plotting graphs/Lesson4.py
+++ 1_Plotting graphs/Lesson4.py
@@ -1,10 +1,17 @@
 import numpy as np
-import matplotlib as ltp
+import matplotlib.pyplot as plt
 
 def graph3():
     """
 
     :return:
     """
-    pass
-graph3()
+    with plt.xkcd():
+        plt.xkcd()
+        plt.pie([70,10, 10, 10], labels=('В комментариях', 'В Ираке', 'В Сирии', 'В Афганистане'))
+        plt.title('Где ведутся самые ожесточенные бои')
+
+
+    plt.show()
+#graph3()
+eval("2 + 3*len('hello')")
