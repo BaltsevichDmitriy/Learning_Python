95d26de67e4c899388b9889c88e58756e52b6c0e
BalsevichDmintriy <Baltsevich@yandex.ru>
2021-02-25 08:40:04 +0700

    Lesson4 finished

--- 1_Plotting graphs/Lesson4.py
+++ 1_Plotting graphs/Lesson4.py
@@ -1,17 +1,13 @@
-import numpy as np
 import matplotlib.pyplot as plt
 
-def graph3():
-    """
 
-    :return:
-    """
+def graph3():
+    """plotting graph input from the terminal """
     with plt.xkcd():
         plt.xkcd()
-        plt.pie([70,10, 10, 10], labels=('В комментариях', 'В Ираке', 'В Сирии', 'В Афганистане'))
+        plt.pie([70, 10, 10, 10], labels=('В комментариях', 'В Ираке', 'В Сирии', 'В Афганистане'))
         plt.title('Где ведутся самые ожесточенные бои')
-
-
     plt.show()
-#graph3()
-eval("2 + 3*len('hello')")
+
+
+eval(input('введите имя функции'))
