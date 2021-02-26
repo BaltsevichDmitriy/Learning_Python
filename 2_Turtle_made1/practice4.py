d9157a6651460935f7e78664cea0b937f3955421
BaltsevichDmitriy <baltsevich@yandex.ru>
2021-02-26 21:46:35 +0700

    Practice4, made a circle, is finished

---
+++ 2_Turtle_made1/practice4.py
@@ -0,0 +1,17 @@
+import turtle as t
+
+
+def circle():
+    """the turtle draws a circle """
+    t.width(3)
+    t.color("green", "orange")
+    t.begin_fill()
+    t.shape('turtle')
+    for i in range(360):
+        t.forward(2)
+        t.left(1)
+    t.end_fill()
+    t.done()
+
+
+circle()
