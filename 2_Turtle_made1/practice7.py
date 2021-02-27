06ee2d8a965d1a2869815f033fac1464ffdfcce3
BaltsevichDmitriy <baltsevich@yandex.ru>
2021-02-27 09:52:49 +0700

    Practice7, create the spiral with N rings finished

--- 2_Turtle_made1/practice7.py
+++ 2_Turtle_made1/practice7.py
@@ -2,29 +2,26 @@ import turtle as t
 import math
 
 
-def spiral():
-    """Рисует спираль с N кольцами"""
-    t.color('green', 'green')
+def spiral(ring=5):
+    """the turtle draws a spiral with N rings"""
+    t.color('blue', 'green')
+    t.shape('turtle')
+    t.width(3)
     t.speed(0)
-    radius = 0
-    ring_total = 10
-    ring_total_n = 0
-    ring = 360
-    ring_now = 0
+    radius = 2
     think = 20
-    while ring_total != ring_total_n:
-        ring_now = 0
-        angl = 0
-        ring_total_n += 1
-        while ring != ring_now:
-            x = radius * math.cos(math.radians(angl))
-            y = radius * math.sin(math.radians(angl))
-            # x1 = radius * math.cos(math.radians(angl + 1))
-            # y1 = radius * math.sin(math.radians(angl + 1))
+    t.left(90)
+    delta = 8
+    for i in range(ring):
+        angle = 0
+        for ii in range(int(360/delta)):
+            x = radius * math.cos(math.radians(angle))
+            y = radius * math.sin(math.radians(angle))
             t.goto(x, y)
-            ring_now += 1
-            angl += 1
-            radius += think / 360
+            angle += delta
+            radius += think / (360/delta)
+            t.left(delta)
+    t.done()
 
 
-spiral()
+spiral(10)
